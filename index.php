9f60c9d99f3d76ef857a420765050150c124e86d
Mel Tajon <meltajon@gmail.com>
2016-08-10 11:33:06 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en-US" prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#" data-theme="A07 ">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+<link rel="stylesheet" media="all" href="assets/normalize.css"/>
+<link rel="stylesheet" media="all" href="assets/style.css"/>
+
+<link rel="dns-prefetch" href="_______"/>
+<link rel="prefetch" href="_______"/>
+
+<title>Nationstar Mortgage</title>
+<meta name="description" content="_______"/>
+
+<meta property="fb:admins" content="_______"/>
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="_______"/>
+<meta property="og:title" content="_______"/>
+<meta property="og:url" content="_______"/>
+<meta property="og:site_name" content="_______"/>
+</head>
+<body>
+
+<header id="header">
+	<div id="header-wrapper" class="wrapper">
+		<div class="td">
+			<a id="home-link" href="/"><img id="logo" alt="Nationstar Mortgage" src="assets/logo-nationstar-mortgage-139x53.png" /></a>
+		</div>
+		<div class="td">
+			<h1 id="page-title">Welcome to Nationstar</h1>
+		</div>
+		<div class="td">
+			<nav id="header-nav">
+				<a id="menu" href="#"><span>Menu</span></a>
+				<a class="header-nav-item" href="#">About Us</a>
+				<a class="header-nav-item" href="#">Customer Review</a>
+			</nav>
+		</div>
+	</div><!-- wrapper -->
+</header>
+<main>
+	<div class="wrapper">
+		<article>
+			<section>
+				<h2>Hi there! We're your new mortgage servicer.</h2>
+				<div class="section-body">
+					<img class="image-right" src="assets/handshake-660x470.jpg" />
+					<p>We're really looking forward to working with you. As America's largest non-bank mortgage servicer, our stability and experience will be your peace of mind. But we always treat each customer as an individual with unique challenges and goals, and we work hard every day to offer unique solutions.</p>
+					<p>That's how we've been able to help hundreds of thousands of people realize their homeownership dreams since 1997.</p>
+					<p>We're honored to have the opportunity to do the same for you.</p>
+				</div><!-- section-body -->
+			</section>
+			<section>
+				<h2>What now?</h2>
+				<div class="section-body">
+					<p>First and foremost, the terms of your loan—including your loan type, rate, and payment due date—are not changing from what they were with your previous servicer.</p>
+					<p>Fifteen days before your loan is officially transferred to Nationstar, your former servicer will mail you a Good-Bye Letter containing important information including your official transfer date.</p>
+					<p>You can start making your <a href="#">payments</a> to Nationstar starting on your transfer date. (But you can keep making payments to your prior servicer for 60 days from your transfer date. They'll forward them to us.)</p>
+					<p>Your official transfer date will probably be on the 1st of the month or the 16th of the month, though may be different. Belower you can learn more about typical transfer timelines.</p>
+				</div><!-- section-body -->
+			</section>
+			<section>
+				<div id="products">
+					<div class="product">
+						<h3 class="product-title">1st of The Month</h3>
+						<div class="product-body">
+							<p>You can register for a Nationstar Online account on the 6th. We will mail you a Welcome Packet on the 15th.</p>
+						</div>
+						<footer class="product-footer">
+							<a class="register-button" href="#">Register Now</a>
+						</footer>
+					</div>
+					<div class="product">
+						<h3 class="product-title">16th of The Month</h3>
+						<div class="product-body">
+							<p>You can register for a Nationstar Online account on the 20th. We will mail you a Welcome Packet on the XXth.</p>
+						</div>
+						<footer class="product-footer">
+							<a class="register-button" href="#">Register Now</a>
+						</footer>
+					</div>
+					<div class="product">
+						<h3 class="product-title">Other Transfer Dates</h3>
+						<div class="product-body">
+							<p>You can register for a Nationstar Online account 5 days after your transfer date. We will mail you a Welcome Packet 15 days after your transfer date.</p>
+						</div>
+						<footer class="product-footer">
+							<a class="register-button" href="#">Register Now</a>
+						</footer>
+					</div>
+				</div><!-- products -->
+				<div id="contact-us">
+					<p><strong>Please feel free to <a href="#">contact us</a> with any questions about your transfer.</strong></p>
+					<small>p.s. Nationstar is launching a brand new website soon! We've spent over a year creating what we think is the best, most customer-friendly online mortgage experience ever. We can't wait for you to see it</small>
+				</div><!-- contact-us -->
+			</section>
+		</article>
+	</div><!-- wrapper -->
+</main>
+<footer id="footer">
+	<div class="wrapper">
+		<div id="footer-panel1">
+			<nav id="footer-nav">
+				<span class="footer-nav-column">
+					<span><a href="#">About Us</a></span>
+					<span><a href="#">Careers</a></span>
+					<span><a href="#">Investor Relations</a></span>
+				</span>
+				<span class="footer-nav-column">
+					<span><a href="#">Contact Us</a></span>
+					<span><a href="#">Business Resources</a></span>
+					<span><a href="#">Press</a></span>
+				</span>
+				<span class="footer-nav-column">
+					<span><a href="#">Terms Of Use</a></span>
+					<span><a href="#">Privacy</a></span>
+					<span><a href="#">Legal</a></span>
+					<span><a href="#">CA, WA &amp; NV Residents</a></span>
+				</span>
+				<span id="footer-nav-column_social">
+					<a href="#"><img src="assets/logo-nationstar-mortgage_grayscale-134x48.png" alt="Nationstar Mortgage" /></a>
+					<div>
+						<a href="#"><img src="assets/icon-facebook-24x24.png" alt="Follow Us on Facebook" /></a>
+						<a href="#"><img src="assets/icon-twitter-24x24.png" alt="Follow Us on Twitter" /></a>
+						<a href="#"><img src="assets/icon-linkedin-24x24.png" alt="Follow Us on LinkedIn" /></a>
+					</div>
+				</span>
+			</nav>
+		</div><!-- footer-panel1 -->
+		<div id="footer-panel2">
+			<div class="footer-panel2-column">
+				<img src="assets/icon-equal-housing-opportunity-47x52.png" alt="Equal Housing Opportunity" />
+				<img src="assets/icon-equal-housing-lender-46x52.png" alt="Equal Housing Lender" />
+			</div><!-- footer-panel2-column -->
+			<div class="footer-panel2-column">
+				<small>
+					&copy; 2016 Nationstar Mortgage LLC. 
+					<span><strong>NMLS:</strong> #2119</span>
+				</small>
+			</div><!-- footer-panel2-column -->
+			<div class="footer-panel2-column">
+				<small>
+					<strong>Customer Service</strong> 
+					<span>888-480-2432</span>
+				</small>
+			</div><!-- footer-panel2-column -->
+		</div>
+	</div><!-- wrapper -->
+</footer>
+
+</body>
